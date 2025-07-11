b227d72247b54a5ed2f8718f9b834151ca88920f
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2025-07-11 11:00:23 +0300

    Tema 1.2

--- ExpenseTracker/ExpenseTracker/obj/Debug/net7.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker/obj/Debug/net7.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70c878c0d9d541cbe99c1f15e63ed91b61b5791")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
