864a1ceff2a388aff90c5cc6cd8e39aa08bb76c3
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2025-07-16 10:30:13 +0300

    Tema 4.1 - Raport cheltuieli si venituri

--- ExpenseTracker/ExpenseTracker/obj/Debug/net7.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker/obj/Debug/net7.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ef4ab66c790846b547389fa787826fbdc0d9fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18063f6fe6bc084b26225d022e9f60168d66496")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
